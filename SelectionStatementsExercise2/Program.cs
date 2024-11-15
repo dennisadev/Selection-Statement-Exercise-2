33ea9ceb9cb5ba5514576ce357f086afdac0363a
Dennis <dennisadev@gmail.com>
2024-11-15 13:21:48 -0500

    inital commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is fun!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is fun!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is fun!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is fun!");
+                    break;
+                case "gym":
+                    Console.WriteLine("Gym is fun!");
+                    break;
+                default:
+                    Console.WriteLine("I don't know about that");
+                    break;
+             }
         }
     }
 }
